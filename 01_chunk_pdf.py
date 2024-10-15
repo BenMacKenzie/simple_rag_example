e935ad25d3f459fec13f37735c3f63e85bc49954
benmackenzie <benmackenzie>
2024-10-15 18:20:22 +0000

    refactor
    doens't work yet

--- 01_chunk_pdf.py
+++ 01_chunk_pdf.py
@@ -128,34 +128,159 @@ if raw_files_df.count() == 0:
 
 # COMMAND ----------
 
+from typing import TypedDict
+from datetime import datetime
+import warnings
+import io
+import traceback
+import os
+from urllib.parse import urlparse
+
+# PDF libraries
+from pypdf import PdfReader
+
+# HTML libraries
+from markdownify import markdownify as md
+import markdownify
+import re
+
+## DOCX libraries
+import pypandoc
+import tempfile
+
+# Schema of the dict returned by `file_parser(...)`
 class ParserReturnValue(TypedDict):
-    doc_parsed_contents: Dict[str, str]
-    parser_status: str
+    # DO NOT CHANGE THESE NAMES - these are required by Evaluation & Framework
+    # Parsed content of the document
+    doc_content: str  # do not change this name
+    # The status of whether the parser succeeds or fails, used to exclude failed files downstream
+    parser_status: str  # do not change this name
+    # Unique ID of the document
+    doc_uri: str  # do not change this name
+
+    # OK TO CHANGE THESE NAMES
+    # Optionally, you can add additional metadata fields here
+    last_modified: datetime
 
 
-def parse_bytes_pypdf(
+# Parser function.  Replace this function to provide custom parsing logic.
+def file_parser(
     raw_doc_contents_bytes: bytes,
+    doc_path: str,
+    modification_time: datetime,
+    doc_bytes_length: int,
 ) -> ParserReturnValue:
+    """
+    Parses the content of a PDF document into a string.
+
+    This function takes the raw bytes of a PDF document and its path, attempts to parse the document using PyPDF,
+    and returns the parsed content and the status of the parsing operation.
+
+    Parameters:
+    - raw_doc_contents_bytes (bytes): The raw bytes of the document to be parsed (set by Spark when loading the file)
+    - doc_path (str): The DBFS path of the document, used to verify the file extension (set by Spark when loading the file)
+    - modification_time (timestamp): The last modification time of the document (set by Spark when loading the file)
+    - doc_bytes_length (long): The size of the document in bytes (set by Spark when loading the file)
+
+    Returns:
+    - ParserReturnValue: A dictionary containing the parsed document content and the status of the parsing operation.
+      The 'doc_content' key will contain the parsed text as a string, and the 'parser_status' key will indicate
+      whether the parsing was successful or if an error occurred.
+    """
     try:
-        pdf = io.BytesIO(raw_doc_contents_bytes)
-        reader = PdfReader(pdf)
+        filename, file_extension = os.path.splitext(doc_path)
 
-        parsed_content = [page_content.extract_text() for page_content in reader.pages]
-        output = {
-            "num_pages": str(len(parsed_content)),
-            "parsed_content": "\n".join(parsed_content),
-        }
+        if file_extension == ".pdf":
+            pdf = io.BytesIO(raw_doc_contents_bytes)
+            reader = PdfReader(pdf)
 
-        return {
-            "doc_parsed_contents": output,
-            "parser_status": "SUCCESS",
-        }
+            parsed_content = [
+                page_content.extract_text() for page_content in reader.pages
+            ]
+
+            parsed_document = {
+                "doc_content": "\n".join(parsed_content),
+                "parser_status": "SUCCESS",
+            }
+        elif file_extension == ".html":
+            from markdownify import markdownify as md
+
+            html_content = raw_doc_contents_bytes.decode("utf-8")
+
+            markdown_contents = md(
+                str(html_content).strip(), heading_style=markdownify.ATX
+            )
+            markdown_stripped = re.sub(r"\n{3,}", "\n\n", markdown_contents.strip())
+
+            parsed_document = {
+                "doc_content": markdown_stripped,
+                "parser_status": "SUCCESS",
+            }
+        elif file_extension == ".docx":
+            with tempfile.NamedTemporaryFile(delete=True) as temp_file:
+                temp_file.write(raw_doc_contents_bytes)
+                temp_file_path = temp_file.name
+                md = pypandoc.convert_file(temp_file_path, "markdown", format="docx")
+
+                parsed_document = {
+                    "doc_content": md.strip(),
+                    "parser_status": "SUCCESS",
+                }
+        else:
+            raise Exception(f"No supported parser for {doc_path}")
+
+        # Extract the required doc_uri
+        # convert from `dbfs:/Volumes/catalog/schema/pdf_docs/filename.pdf` to `Volumes/catalog/schema/pdf_docs/filename.pdf`
+        modified_path = urlparse(doc_path).path.lstrip('/')
+        parsed_document["doc_uri"] = modified_path
+
+
+        # Add the modified time
+        parsed_document["last_modified"] = modification_time
+
+        return parsed_document
+
     except Exception as e:
-        warnings.warn(f"Exception {e} has been thrown during parsing")
+        status = f"An error occurred: {e}\n{traceback.format_exc()}"
+        warnings.warn(status)
         return {
-            "doc_parsed_contents": {"num_pages": "", "parsed_content": ""},
-            "parser_status": f"ERROR: {e}",
+            "doc_content": "",
+            "parser_status": f"ERROR: {status}",
         }
+
+# COMMAND ----------
+
+# MAGIC %run ./utils/typed_dicts_to_spark_schema
+
+# COMMAND ----------
+
+from utils.file_loading import load_files_to_df, apply_parsing_udf
+
+# COMMAND ----------
+
+raw_files_df = load_files_to_df(
+    spark=spark,
+    source_path=source_path,
+)
+
+parsed_files_df = apply_parsing_udf(
+    raw_files_df=raw_files_df,
+    # Modify this function to change the parser, extract additional metadata, etc
+    parse_file_udf=file_parser,
+    # The schema of the resulting Delta Table will follow the schema defined in ParserReturnValue
+    parsed_df_schema=typed_dicts_to_spark_schema(ParserReturnValue)
+)
+
+# Write to a Delta Table
+parsed_files_df.write.mode("overwrite").option(
+    "overwriteSchema", "true"
+).saveAsTable(DOCS_DELTA_TABLE)
+
+# Display for debugging
+print(f"Parsed {parsed_files_df.count()} documents.")
+parsed_files_df.display()
+
+
 
 # COMMAND ----------
 
@@ -163,22 +288,6 @@ def parse_bytes_pypdf(
 # MAGIC ### Parser UDF
 # MAGIC
 # MAGIC This UDF wraps your parser into a UDF so Spark can parallelize the data processing.
-
-# COMMAND ----------
-
-parser_udf = func.udf(
-    parse_bytes_pypdf,
-    returnType=StructType(
-        [
-            StructField(
-                "doc_parsed_contents",
-                MapType(StringType(), StringType()),
-                nullable=True,
-            ),
-            StructField("parser_status", StringType(), nullable=True),
-        ]
-    ),
-)
 
 # COMMAND ----------
 
